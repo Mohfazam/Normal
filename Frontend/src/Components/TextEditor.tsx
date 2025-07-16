933620e8ec2c034ba7613039ad3c875c588f6eae
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 17:08:03 +0530

    installation

--- Frontend/src/Components/TextEditor.tsx
+++ Frontend/src/Components/TextEditor.tsx
@@ -1,4 +1,7 @@
-import Editor from "react-medium-editor"
+import Editor from "react-medium-editor";
+
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/default.css';
 
 export const TextEditor = () => {
     return (
