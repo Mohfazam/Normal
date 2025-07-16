302c1a31753ce4b785499422e728a744216b449b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-17 02:36:06 +0530

    All Posts header

--- Frontend/src/Pages/AllPosts.tsx
+++ Frontend/src/Pages/AllPosts.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "../Components/Navbar"
+import { PostCard } from "../Components/PostCard"
 
 export const AllPosts = () => {
     return(
@@ -9,12 +10,16 @@ export const AllPosts = () => {
             
             
             <div className="flex flex-col justify-center items-center mb-16">
-                <div className="text-4xl font-bold text-gray-900 font-serif m-4 leading-tight">
+                <div className="text-5xl font-bold text-gray-900 font-serif m-4 leading-tight">
                     <h1>Stories</h1>
                 </div>
                 <div className="text-wrap text-center w-108">
-                    <span className="text-lg text-gray-600 max-w-xl mx-auto leading-relaxed">Discover insights about web development, design, and modern technology trends.</span>
+                    <span className="text-lg text-gray-600 max-w-xl mx-auto leading-relaxed"><strong>Normal</strong> a clone of medium with post addition, updation and veiw functionalities. assignment for <strong>CODING-JUNIOR</strong></span>
                 </div>
+            </div>
+
+            <div>
+                <PostCard />
             </div>
         </div>
     )
