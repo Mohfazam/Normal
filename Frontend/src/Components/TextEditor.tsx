4fa07eed2bcb48091562812a505d7e2d60627fa7
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 17:02:59 +0530

    init

---
+++ Frontend/src/Components/TextEditor.tsx
@@ -0,0 +1,10 @@
+import Editor from "react-medium-editor"
+
+export const TextEditor = () => {
+    return (
+        <div>
+            {Editor}
+            Text Editor
+        </div>
+    )    
+}
