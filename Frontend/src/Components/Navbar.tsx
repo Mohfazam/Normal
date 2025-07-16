f1b7bae022fea8592476e720c70abb0d5ee3116e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 19:49:52 +0530

    navBar Structure

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -2,6 +2,18 @@ export const Navbar = () => {
     return(
         <div className="w-full m-4">
             <h1 className="text-4xl">Normal</h1>
+
+            <div>
+                <button>Home</button>
+            </div>
+
+            <div>
+                <button>AllBlogs</button>
+            </div>
+
+            <div>
+                <button>New Blog</button>
+            </div>
         </div>
     )
 }
