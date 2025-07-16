21ad81d2146c5fe33f2f894f512580aafa37a3c1
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 19:58:18 +0530

    navBar Structure 4

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,16 +1,16 @@
 export const Navbar = () => {
     return (
-        <div className="sticky top-0 z-50 w-full bg-white border-b border-gray-200 shadow-sm">
-            <div>
-                <h1 className="text-4xl">Normal</h1>
+        <div className="sticky top-0 z-50 w-full bg-white border-b border-gray-200 shadow-sm p-4 pb-6">
+            <div className="flex">
+                <h1 className="text-4xl w-4xl text-gray-900 font-bold">Normal</h1>
 
-                <div>
+                <div className="flex gap-22 justify-between items-center">
                     <div>
                         <button>Home</button>
                     </div>
 
                     <div>
-                        <button>AllBlogs</button>
+                        <button>All Blogs</button>
                     </div>
 
                     <div>
