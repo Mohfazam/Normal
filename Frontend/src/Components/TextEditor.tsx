01530cd4481e699b3393474ae5e344f0396a46c6
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 17:23:40 +0530

    tiptap

--- Frontend/src/Components/TextEditor.tsx
+++ Frontend/src/Components/TextEditor.tsx
@@ -1,13 +1,34 @@
 import Editor from "react-medium-editor";
-
 import 'medium-editor/dist/css/medium-editor.css';
 import 'medium-editor/dist/css/themes/default.css';
+import { useState } from "react";
 
 export const TextEditor = () => {
-    return (
-        <div>
-            {Editor}
-            Text Editor
-        </div>
-    )    
-}
+  const [text, setText] = useState("User content");
+
+  const handleChange = (newText: string) => {
+    setText(newText);
+  };
+
+  return (
+    <div>
+      <h1>React Medium Editor Demo</h1>
+
+      <h2>Editable Content</h2>
+
+      <Editor
+        tag="div"
+        text={text}
+        onChange={handleChange}
+        options={{
+          toolbar: {
+            buttons: ["bold", "italic", "underline"]
+          },
+        }}
+      />
+
+      <h3>Output Preview:</h3>
+      <div dangerouslySetInnerHTML={{ __html: text }} />
+    </div>
+  );
+};
