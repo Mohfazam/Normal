b3fa8cf0676463a5e597f1087d162b8a5a8b2194
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-17 02:31:24 +0530

    All Posts header

--- Frontend/src/Pages/AllPosts.tsx
+++ Frontend/src/Pages/AllPosts.tsx
@@ -8,12 +8,12 @@ export const AllPosts = () => {
             </div>
             
             
-            <div>
-                <div>
+            <div className="flex flex-col justify-center items-center mb-16">
+                <div className="text-4xl font-bold text-gray-900 font-serif m-4 leading-tight">
                     <h1>Stories</h1>
                 </div>
-                <div>
-                    <span>Discover insights about web development, design, and modern technology trends.</span>
+                <div className="text-wrap text-center w-108">
+                    <span className="text-lg text-gray-600 max-w-xl mx-auto leading-relaxed">Discover insights about web development, design, and modern technology trends.</span>
                 </div>
             </div>
         </div>
