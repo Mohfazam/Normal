22459024cbf56ba36a7b21ef31ab56a8494abeb6
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-17 02:26:48 +0530

    Post card

--- Frontend/src/Pages/AllPosts.tsx
+++ Frontend/src/Pages/AllPosts.tsx
@@ -3,8 +3,19 @@ import { Navbar } from "../Components/Navbar"
 export const AllPosts = () => {
     return(
         <div>
-            <Navbar />
-            ALL BLOGS
+            <div>
+                <Navbar />
+            </div>
+            
+            
+            <div>
+                <div>
+                    <h1>Stories</h1>
+                </div>
+                <div>
+                    <span>Discover insights about web development, design, and modern technology trends.</span>
+                </div>
+            </div>
         </div>
     )
 }
