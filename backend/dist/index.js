25412982067bc3de1293440f4641b39023f427ff
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-17 00:17:48 +0530

    add Post

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,10 +1,21 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const client_1 = require("@prisma/client");
+const client = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
@@ -13,6 +24,27 @@ app.get("/health", (req, res) => {
         Message: "Backend Up and Running"
     });
 });
+app.post("/addPost", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, body } = req.body;
+    try {
+        const response = yield client.post.create({
+            data: {
+                title: title,
+                body: body,
+            }
+        });
+        res.status(200).json({
+            Message: "Post Added Successfully",
+            response
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            Message: "Something wrong",
+            error: error
+        });
+    }
+}));
 app.listen(3000, () => {
     console.log("Server Running at port 3000");
 });
--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -1,5 +1,8 @@
 import  express  from "express";
 import cors from "cors"
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
 app.use(cors());
@@ -10,6 +13,29 @@ app.get("/health", (req, res) => {
     res.status(200).json({
         Message: "Backend Up and Running"
     });
+});
+
+app.post("/addPost", async (req, res) => {
+    const {title, body} = req.body;
+
+    try{
+        const response = await client.post.create({
+        data:{
+            title: title,
+            body: body,
+        }
+    });
+
+    res.status(200).json({
+        Message: "Post Added Successfully",
+        response
+    });
+    } catch(error){
+        res.status(500).json({
+            Message: "Something wrong",
+            error: error
+        });
+    }
 });
 
 app.listen(3000, () => {
