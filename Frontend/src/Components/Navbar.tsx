38e5855d4cd81d7e66b6abdc1d2ec8209083b777
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-16 19:53:28 +0530

    navBar Structure 2

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,9 +1,11 @@
 export const Navbar = () => {
     return(
-        <div className="w-full m-4">
-            <h1 className="text-4xl">Normal</h1>
+        <div className="sticky top-0 z-50 w-full bg-white border-b border-gray-200 shadow-sm">
+            <div>
+                <h1 className="text-4xl">Normal</h1>
 
             <div>
+                <div>
                 <button>Home</button>
             </div>
 
@@ -13,6 +15,8 @@ export const Navbar = () => {
 
             <div>
                 <button>New Blog</button>
+            </div>
+            </div>
             </div>
         </div>
     )
