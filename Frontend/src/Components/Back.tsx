efc22a7c6db3e4f18e3c9a49d334115b42308984
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-18 00:09:06 +0530

    View Post

--- Frontend/src/Components/Back.tsx
+++ Frontend/src/Components/Back.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "lucide-react"
 export const Back = () => {
     return(
         <div className="border-b border-gray-200 " >
-            <div className="flex max-w-4xl mx-auto px-6 py-6 gap-2 items-center justify-start">
+            <div className="flex max-w pl-10 mx-auto px-6 py-6 gap-2 items-center justify-start">
                 <div className="flex justify-start  h-[36px] p-3 gap-2 hover:cursor-pointer items-center hover:bg-gray-100 hover:rounded-xl">
                     <ArrowLeft/>
                 <span className="text-lg font-medium">Back</span>
--- Frontend/src/Pages/ViewPost.tsx
+++ Frontend/src/Pages/ViewPost.tsx
@@ -1,7 +1,33 @@
+import { useParams } from "react-router-dom"
 import { Back } from "../Components/Back"
 import { Navbar } from "../Components/Navbar"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ViewPost = () => {
+
+    const { id } = useParams();
+
+    const [postTitle, setPostTitle] = useState("");
+    const [postBody, setPostBody] = useState("");
+    const [createdAt, setcreatedAt] = useState("");
+
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try{
+                const post = await axios.get(`https://normalbackend.vercel.app/getPost?id=${id}`);
+                setPostTitle(post.data.post.title);
+                setPostBody(post.data.post.body);
+                setcreatedAt(post.data.createdAt);
+            } catch(error){
+                console.log("Something went wrong: ", error);
+            }
+        }
+
+        fetchPost();
+    }, [])
+
     return(
         <div>
             <div>
@@ -9,6 +35,25 @@ export const ViewPost = () => {
             </div>
             <div>
                 <Back />
+            </div>
+
+            {/* main */}
+            <div className="max-w-3xl mx-auto px-6 py-12">
+                <div>
+                    <p className="text-gray-500 text-sm">Published on {createdAt}</p>
+                </div>
+
+                <div className="w-4xl py-8 text-4xl">
+                    <div dangerouslySetInnerHTML={{__html: postTitle}} />
+                        
+                    
+                </div>
+
+                <div className="p-4 h-fit w-4xl">
+                    <div dangerouslySetInnerHTML={{__html: postBody}} />
+                        
+                </div>
+
             </div>
         </div>
     )
