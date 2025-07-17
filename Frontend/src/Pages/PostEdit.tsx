ae27bd388317ff6a14f6c348fe88f338580fc403
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-17 21:28:26 +0530

    Post Update

--- Frontend/src/Pages/PostEdit.tsx
+++ Frontend/src/Pages/PostEdit.tsx
@@ -1,7 +1,63 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Navbar } from '../Components/Navbar';
+import { TextEditor } from '../Components/TextEditor';
+import { useNavigate } from 'react-router-dom';
+
 export const PostEdit = () => {
-    return(
-        <div>
-            Post Edit
+
+    const [postTitlefetched, setPostTitle] = useState("");
+    const [postBody, setPostBody] = useState("");
+
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+
+
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const postdata = await axios.get(`https://normalbackend.vercel.app/getPost?id=${id}`);
+                setPostTitle(postdata.data.post.title);
+                setPostBody(postdata.data.post.body);
+            } catch (error) {
+                console.log("Error catched:", error);
+            }
+        }
+
+        fetchPost();
+    }, []);
+
+    const handlePostUpdate = async (content: { title: string; body: string }) => {
+
+        const updatedPost = {
+            title: content.title,
+            body: content.body,
+            id: id
+        };
+
+        try{
+            const response = await axios.put("https://normalbackend.vercel.app/updatePost", updatedPost);
+            alert("Post Updated");
+            console.log(response);
+            navigate(`/ViewPost/:${id}`);
+        } catch(error){
+            console.log("Something went wrong. error:", error);
+        }
+
+
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <TextEditor initialTitle={postTitlefetched} initialBody={postBody} editable={true} onSave={handlePostUpdate}/>
+            </div>
         </div>
     )
 }
